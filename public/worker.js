c51780653da38eb2e6abab7bd052e3cf606b7e47
SkiBall <divye11@gmail.com>
2021-03-23 15:56:05 +0530

    Added logs

--- public/worker.js
+++ public/worker.js
@@ -1,6 +1,5 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v2';
-const assets = [
+var CACHE_NAME = 'pwa-todo-manager';
+const urlsToCache = [
   '/',
   '/index.html',
   '/src/App.js',
@@ -8,60 +7,99 @@ const assets = [
   '/src/index.js',
   '/src/index.css',
   '/src/App.test.js',
-  '/src/Containers/Home/Home.js',
   'https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min.js',
 ];
 
-// install event
-self.addEventListener('install', (evt) => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
+// // install event
+// self.addEventListener('install', (evt) => {
+//   //console.log('service worker installed');
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       console.log('caching shell assets');
+//       cache.addAll(assets);
+//     }),
+//   );
+// });
+
+// // activate event
+// self.addEventListener('activate', (evt) => {
+//   //console.log('service worker activated');
+//   evt.waitUntil(
+//     caches.keys().then((keys) => {
+//       //console.log(keys);
+//       return Promise.all(
+//         keys
+//           .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+//           .map((key) => caches.delete(key)),
+//       );
+//     }),
+//   );
+// });
+
+// // fetch events
+// self.addEventListener('fetch', (evt) => {
+//   if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+//     evt.respondWith(
+//       caches
+//         .match(evt.request)
+//         .then((cacheRes) => {
+//           return (
+//             cacheRes ||
+//             fetch(evt.request).then((fetchRes) => {
+//               return caches.open(dynamicCacheName).then((cache) => {
+//                 cache.put(evt.request.url, fetchRes.clone());
+//                 // check cached items size
+//                 limitCacheSize(dynamicCacheName, 15);
+//                 return fetchRes;
+//               });
+//             })
+//           );
+//         })
+//         .catch(() => {
+//           if (evt.request.url.indexOf('.html') > -1) {
+//             return caches.match('/pages/fallback.html');
+//           }
+//         }),
+//     );
+//   }
+// });
+
+// Install a service worker
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     }),
   );
 });
 
-// activate event
-self.addEventListener('activate', (evt) => {
-  //console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then((keys) => {
-      //console.log(keys);
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keys
-          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
-          .map((key) => caches.delete(key)),
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
       );
     }),
   );
-});
-
-// fetch events
-self.addEventListener('fetch', (evt) => {
-  if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then((cacheRes) => {
-          return (
-            cacheRes ||
-            fetch(evt.request).then((fetchRes) => {
-              return caches.open(dynamicCacheName).then((cache) => {
-                cache.put(evt.request.url, fetchRes.clone());
-                // check cached items size
-                limitCacheSize(dynamicCacheName, 15);
-                return fetchRes;
-              });
-            })
-          );
-        })
-        .catch(() => {
-          if (evt.request.url.indexOf('.html') > -1) {
-            return caches.match('/pages/fallback.html');
-          }
-        }),
-    );
-  }
 });
