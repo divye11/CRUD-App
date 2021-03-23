d2067ae07e8835bdbfcaac8e21dc270265622f44
SkiBall <divye11@gmail.com>
2021-03-23 17:56:35 +0530

    Removed material ui

--- public/worker.js
+++ public/worker.js
@@ -10,7 +10,7 @@ const assets = [
   '/src/index.js',
   '/src/index.css',
   '/src/App.test.js',
-  'https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min.js',
+  // 'https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min.js',
 ];
 
 // install event
